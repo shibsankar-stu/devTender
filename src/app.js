194c3d4c7c11b55c5f5e626cd25d7d4c691e1af9
shibsankar-stu <dasshibsankar94732@gmail.com>
2025-04-27 02:27:50 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.send("Home");
-})
 
+app.get("/user/:uid", (req, res) => {
+    
+    res.send("Hello from /user get route.");
+})
+app.post("/user", (req, res) => {
+    res.send("Hello from /user post route.");
+})
 app.use("/test", (req, res) => {
     res.send("Hello from /test route.");
 })
-
-
-
 const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
     console.log("Server is running on port ", PORT);
 })
