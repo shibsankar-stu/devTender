7ac18c91a7cfcf267bec0652dcc4d887a37849c1
shibsankar-stu <dasshibsankar94732@gmail.com>
2025-04-28 02:50:13 +0530

    update schema

--- src/model/user.js
+++ src/model/user.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
+    firstName: { type: String, required: true, trim: true, maxLength: 30 },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     age: { type: Number, required: true },
     bio: { type: String, required: true },
-    interests: [{ type: String }],
+    skills: [{ type: String }],
     image: { type: String, default: "default.jpg" },
     tokens: [{ token: { type: String, required: true } }]
-});
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User
